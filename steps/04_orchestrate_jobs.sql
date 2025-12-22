b288b6b4099e2530065dd36c43a38f8eec01f8b5
dai2-metrodata <rinno.bagus@mii.co.id>
2025-12-22 10:34:35 +0000

    parametrize pipeline

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -1,5 +1,6 @@
 use role accountadmin;
-use schema quickstart_prod.gold;
+use schema quickstart_{{environment}}.gold;
+
 
 ALTER ACCOUNT SET CORTEX_ENABLED_CROSS_REGION = 'AWS_US';
 
@@ -18,7 +19,8 @@ create or alter table vacation_spots (
   , aquarium_cnt int
   , zoo_cnt int
   , korean_restaurant_cnt int
-) data_retention_time_in_days = 1;
+) data_retention_time_in_days = {{retention_time}};
+
 
 
 -- task to merge pipeline results into target table
